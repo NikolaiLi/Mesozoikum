7ff723c5668072948083f33d9be39bac6d74b123
Ibrainium <ibrahim08780878@gmail.com>
2024-04-17 21:37:47 +0200

    Check discord gruppen (17/4/2024)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene("Game Scene");
+    }
+    public void PlayGame3()
+    {
+        SceneManager.LoadScene("LoseScreen");
+    }
+    public void PlayGame4()
+    {
+        SceneManager.LoadScene("WinScreen");
+    }
+    public void PlayGame5()
+    {
+        SceneManager.LoadScene("Map");
+    }
+}
